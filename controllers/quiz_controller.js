c05da736f7dc65e26df4906f4207f3f31b5ee974
rjprieto <ramonj.prieto.exts@juntadeandalucia.es>
2015-06-08 11:47:45 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -38,7 +38,6 @@ exports.index = function(req, res) {
 //GET /quizes/show
 exports.show = function (req, res) {
 	res.render('quizes/show', {quiz : req.quiz});
-
 }
 
 //GET /quizes/answer
@@ -48,4 +47,19 @@ exports.answer = function (req, res) {
 		resultado = 'Correcto';
 	}
 	res.render('quizes/answer', {quiz:req.quiz, respuesta : resultado});
+}
+
+//GET /quizes/new
+exports.new = function (req, res) {
+	var quiz = models.Quiz.build( {pregunta:'Pregunta', respuesta:'Respuesta'} );
+	res.render('quizes/new', {quiz:quiz});
+}
+
+//POST /quizes/create
+exports.create = function (req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+	
+	quiz.save( {fields: ["pregunta", "respuesta"]}).then( function() {
+		res.redirect("/quizes");
+	});
 }
