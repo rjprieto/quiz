a6ed6413ee19bfa1c89354ca2d1f6f40bd913687
rjprieto <ramonj.prieto.exts@juntadeandalucia.es>
2015-06-11 17:29:01 +0200

    Añadir una página de estadisticas

---
+++ controllers/statistics_controller.js
@@ -0,0 +1,39 @@
+﻿var models = require('../models/models.js');
+
+//GET /quizes/statistics
+exports.show = function (req, res) {
+	var numPreguntas = 0;
+	var numComentarios = 0;
+	var mediaComentarios = 0.0;
+	var numSinComentarios = 0;
+	var numConComentarios = 0;
+
+	models.Quiz.count().then( function (num) {
+		numPreguntas = num;
+		models.Comment.count().then( function (num) {
+			numComentarios = num;
+
+			if (numPreguntas > 0) {
+				mediaComentarios = numComentarios / numPreguntas;
+			}
+
+			models.Quiz.findAll(
+							{
+								include: [{model: models.Comment, required: true }],
+								group: 'Quizzes.id'
+							}
+				).then( function (result) {
+				
+					numConComentarios = result.length;
+					numSinComentarios = numPreguntas - numConComentarios;
+					
+					res.render('quizes/statistics', { data : [numPreguntas,
+												numComentarios,
+												mediaComentarios,
+												numSinComentarios,
+												numConComentarios] , errors: []});
+			}).catch(function(error) { next(error) });
+		}).catch(function(error) { next(error) });
+	}).catch(function(error) { next(error) });
+
+}
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
+var statisticsController = require('../controllers/statistics_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -40,5 +41,8 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
+
+//Estadísticas
+router.get('/quizes/statistics', statisticsController.show);
 
 module.exports = router;
