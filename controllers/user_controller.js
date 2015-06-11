478c8ff694242edcc513eded21e1ee904bc0dce5
rjprieto <ramonj.prieto.exts@juntadeandalucia.es>
2015-06-11 13:04:31 +0200

    Autenticación

---
+++ controllers/user_controller.js
@@ -0,0 +1,17 @@
+﻿var users = { 	admin: {id:1, username:'admin', password:'1234'},
+				ramon: {id:2, username:'ramon', password:'1234'}
+ };
+
+exports.autenticar = function (login, password, callback) {
+	if (users[login]) {
+		if (password === users[login].password) {
+			callback(null, users[login]);
+		}
+		else {
+			callback(new Error('Password erróneo'));
+		}
+	}
+	else {
+		callback(new Error('No existe el usuario'));
+	}
+};
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 //Controladores
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -23,6 +24,13 @@ router.get('/quizes/show', quizController.show);
 //Autoload de todas las rutas que reciban el parámetro :quizId
 router.param('quizId', quizController.load);
 
+//Rutas de sesión
+router.get('/login', sessionController.new);
+router.post('/login', sessionController.create);
+router.get('/logout', sessionController.destroy);
+
+
+//Rutas de Quizes
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
@@ -34,6 +42,7 @@ router.put('/quizes/:quizId(\\d+)', quizController.update);
 
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
+//Rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 
