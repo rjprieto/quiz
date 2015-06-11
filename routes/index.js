958138cc605bc6fa52e8d2b4c2f9d8cb5b8bc6cb
rjprieto <ramonj.prieto.exts@juntadeandalucia.es>
2015-06-11 13:35:49 +0200

    Moderar comentarios

--- routes/index.js
+++ routes/index.js
@@ -15,8 +15,9 @@ router.get('/author', function(req, res) {
   res.render('author', {errors: []});
 });
 
-//Autoload de todas las rutas que reciban el parámetro :quizId
+//Autoload de todas las rutas que reciban el parámetro :quizId y :commentId
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load);
 
 //Rutas de sesión
 router.get('/login', sessionController.new);
@@ -38,5 +39,6 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 //Rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
